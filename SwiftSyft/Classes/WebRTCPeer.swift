dc890674d14c00407e80a6dcf07fba5d42141371
mjjimenez <mjjimenez87@gmail.com>
2020-01-26 17:59:38 +0800

    Added logic to either create data channel when the RTCPeerConnection is the initiator (to create offer) or the receiver (to receive offer).

--- SwiftSyft/Classes/WebRTCPeer.swift
+++ SwiftSyft/Classes/WebRTCPeer.swift
@@ -1,18 +1,39 @@
 import Foundation
 import WebRTC
 
+enum WebRTCConnectionType {
+    case initiator
+    case receiver
+}
+
 /// Wraps a peer connection and its data channel. Allows other observers
 /// to observer peer connection and data channel events.
 private class WebRTCPeer: NSObject {
 
     let rtcPeerConnection: RTCPeerConnection
-    var rtcDataChannel: RTCDataChannel?
+    var rtcDataChannel: RTCDataChannel? {
+        didSet {
+            self.rtcDataChannel?.delegate = self
+        }
+    }
     var iceCandidates: [RTCIceCandidate] = []
+    var connectionType: WebRTCConnectionType
 
-    init(rtcPeerConnection: RTCPeerConnection) {
+    init(rtcPeerConnection: RTCPeerConnection, connectionType: WebRTCConnectionType) {
         self.rtcPeerConnection = rtcPeerConnection
+        self.connectionType = connectionType
         super.init()
         self.rtcPeerConnection.delegate = self
+
+        switch connectionType {
+        case .initiator:
+            let dataChannelConfig = RTCDataChannelConfiguration()
+            self.rtcDataChannel = self.rtcPeerConnection.dataChannel(forLabel: "dataChannel",
+                                                                     configuration: dataChannelConfig)
+            self.rtcDataChannel?.delegate = self
+        case .receiver:
+            break
+        }
     }
 
 }
@@ -52,7 +73,7 @@ extension WebRTCPeer: RTCPeerConnectionDelegate {
     }
 
     func peerConnection(_ peerConnection: RTCPeerConnection, didOpen dataChannel: RTCDataChannel) {
-
+        self.rtcDataChannel = dataChannel
     }
 }
 
