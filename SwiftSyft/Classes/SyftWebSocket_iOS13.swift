bba8091bec6e69b020dcd8354b54e976e29c384f
Madalin Mamuleanu <mamuleanu.madalin@gmail.com>
2020-02-03 20:55:22 +0200

    Added SyftWebSocket class for iOS 13

---
+++ SwiftSyft/Classes/SyftWebSocket_iOS13.swift
@@ -0,0 +1,15 @@
+//
+//  SyftWebSocket_iOS13.swift
+//  Pods-SwiftSyft_Example
+//
+//  Created by Madalin Mamuleanu on 03/02/2020.
+//
+
+import Foundation
+
+public class SyftWebSocketIOS13 {
+    
+    
+    
+    
+}
