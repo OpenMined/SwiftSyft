62beecd7381ccae3342f1830c837df9622f12e83
mjjimenez <mjjimenez87@gmail.com>
2020-02-04 17:54:31 +0800

    Initial implementation of SignallingClient

--- SwiftSyft/Protocols/SignallingClientProtocol.swift
+++ SwiftSyft/Protocols/SignallingClientProtocol.swift
@@ -9,9 +9,13 @@ import Foundation
 
 /// Holds web socket conection logic. Handles socket message serialization/deserialization
 protocol SignallingClientProtocol {
-    init(url: SocketClientProtocol)
 
     func connect()
-    func send()
+    func disconnect()
+    func send(_ message: SignallingMessages) throws
 
+}
+
+protocol SignallingClientDelegate: class {
+    func didReceive(_ message: SignallingMessages)
 }
