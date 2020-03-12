fb96a99028848c02fb59ebeaa0ede125b64a4abf
Madalin Mamuleanu <mamuleanu.madalin@gmail.com>
2020-03-12 23:02:28 +0200

    pingChecker implementation

--- SwiftSyft/Classes/PingChecker.swift
+++ SwiftSyft/Classes/PingChecker.swift
@@ -8,4 +8,25 @@
 
 import Foundation
 
+public class PingChecker: NSObject {
+    
+    static let singletonPingChecker = PingChecker()
+
+    private var pingResultCallback: (String?)->()?
+    private var simplePingClient: SimplePing?
+    private var referenceDate: Date?
+
+    public static func pingHostname(hostname: String, andResultCallback callback: (String?)->()?) {
+        singletonPingChecker.pingHostname(hostname: hostname, andResultCallback: callback)
+    }
+
+    public func pingHostname(hostname: String, andResultCallback callback: (String?)->()?) {
+        resultCallback = callback
+        simplePingClient = SimplePing(hostName: hostname)
+        simplePingClient?.delegate = self
+        simplePingClient?.start()
+    }
+}
+
+
 
