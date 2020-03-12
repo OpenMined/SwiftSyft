0e1338584e1a985972594cf3d04ccb63cc337cd8
Madalin Mamuleanu <mamuleanu.madalin@gmail.com>
2020-03-12 23:08:36 +0200

    SimplePing delegate methods

--- SwiftSyft/Classes/PingChecker.swift
+++ SwiftSyft/Classes/PingChecker.swift
@@ -28,5 +28,37 @@ public class PingChecker: NSObject {
     }
 }
 
+extension PingChecker: SimplePingDelegate {
+    
+    // Simple Ping delegate methods
+    
+    public func simplePing(_ pinger: SimplePing, didStartWithAddress address: Data) {
+        pinger.send(with: nil)
+    }
+    public func simplePing(_ pinger: SimplePing, didFailWithError error: Error) {
+        resultCallback?(nil)
+    }
 
+    public func simplePing(_ pinger: SimplePing, didSendPacket packet: Data, sequenceNumber: UInt16) {
+        dateReference = Date()
+    }
+
+    public func simplePing(pinger: SimplePing!, _ didFailToSendPacket: NSData!, error: NSError!) {
+        pinger.stop()
+        resultCallback?(nil)
+    }
+
+    public func simplePing(_ pinger: SimplePing, didReceiveUnexpectedPacket packet: Data) {
+        pinger.stop()
+        resultCallback?(nil)
+    }
+
+    public func simplePing(_ pinger: SimplePing, didReceivePingResponsePacket packet: Data, sequenceNumber: UInt16) {
+          pinger.stop()
+          guard let reference_date = referenceDate else { return }
+          //timeIntervalSinceDate returns seconds, so we convert to milis
+          let latency = Date().timeIntervalSince(reference_date) * 1000
+          resultCallback?(String(format: "%.f", latency))
+    }
+}
 
