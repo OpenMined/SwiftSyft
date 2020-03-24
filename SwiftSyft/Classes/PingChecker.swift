8e5a2de2bc356306b8c24ee84f5543bbcf16f5b0
Madalin Mamuleanu <mamuleanu.madalin@gmail.com>
2020-03-24 11:42:51 +0200

    Updated after review: Added a new method that returns a Future from the Combine framework. Updated the tests. Added comments for each public methods.

--- SwiftSyft/Classes/PingChecker.swift
+++ SwiftSyft/Classes/PingChecker.swift
@@ -1,4 +1,5 @@
 import Foundation
+import Combine
 
 internal typealias PingCheckerCallback = (UInt16?) -> Void
 
@@ -9,11 +10,38 @@ internal class PingChecker: NSObject {
     private var simplePingClient: SimplePing?
     private var referenceDate: Date?
 
-    public static func pingHostname(hostname: String, andResultCallback callback: PingCheckerCallback?) {
-        singletonPingChecker.pingHostname(hostname: hostname, andResultCallback: callback)
+    /// This method checks the latency between the device and a hostname.
+    ///
+    /// - Important: The latency is computed in miliseconds (ms).
+    ///
+    /// - parameters:
+    ///     - hostname: The hostname to ping. It can be a domain or an IP Address.
+    ///     - resultCallback: The result callback. Check PingCheckerCallback for the callback type.
+    public static func pingHostname(hostname: String, resultCallback callback: PingCheckerCallback?) {
+        singletonPingChecker.pingHostname(hostname: hostname, resultCallback: callback)
     }
-
-    public func pingHostname(hostname: String, andResultCallback callback: PingCheckerCallback?) {
+    /// This method checks the latency between the device and a hostname.
+    ///
+    ///  The code was written with the help of: https://www.vadimbulavin.com/asynchronous-programming-with-future-and-promise-in-swift-with-combine-framework/
+    /// - Important: The latency is computed in miliseconds (ms).
+    ///
+    /// - parameters:
+    ///     - hostname: The hostname to ping. It can be a domain or an IP Address.
+    /// - returns:
+    ///   A publisher that produces one UInt16 value or fails if the hostname is unreachable.
+    public static func pingHostname(hostname: String) -> Future<UInt16, Error> {
+         let pingFuture = Future<UInt16, Error> { promise in
+                 pingHostname(hostname: hostname) { latency in
+                     guard let latencyMS = latency else {
+                         promise(.failure(PingCheckerError.networkUnreachable))
+                         return
+                     }
+                     promise(.success(latencyMS))
+                 }
+         }
+         return pingFuture
+     }
+    private func pingHostname(hostname: String, resultCallback callback: PingCheckerCallback?) {
         pingResultCallback = callback
         simplePingClient = SimplePing(hostName: hostname)
         simplePingClient?.delegate = self
@@ -49,5 +77,8 @@ extension PingChecker: SimplePingDelegate {
        guard let referencedate = referenceDate else { return }
        let latency = UInt16(Date().timeIntervalSince(referencedate) * 1000)
        pingResultCallback?(UInt16(latency))
+    }
+    enum PingCheckerError: Error {
+        case networkUnreachable
     }
 }
--- Tests/PingCheckerTests.swift
+++ Tests/PingCheckerTests.swift
@@ -1,5 +1,6 @@
 import Foundation
 import XCTest
+import Combine
 @testable import SwiftSyft
 
 class PingCheckerTests: XCTestCase {
@@ -15,7 +16,7 @@ class PingCheckerTests: XCTestCase {
     func testPingWithAKnownHost() {
     
         let expectation = XCTestExpectation(description: "Running test with \(KnownHost)")
-        PingChecker.pingHostname(hostname: KnownHost, andResultCallback: {latency in
+        PingChecker.pingHostname(hostname: KnownHost, resultCallback: {latency in
             
             guard let latencyMS = latency else { return }
             XCTAssertTrue(latencyMS > 0)
@@ -27,7 +28,7 @@ class PingCheckerTests: XCTestCase {
     func testPingWithNonExistingHost() {
 
        let expectation = XCTestExpectation(description: "Running test with \(UnknownHost)")
-       PingChecker.pingHostname(hostname: UnknownHost, andResultCallback: {latency in
+       PingChecker.pingHostname(hostname: UnknownHost, resultCallback: {latency in
            XCTAssertNil(latency)
            expectation.fulfill()
            
@@ -40,10 +41,42 @@ class PingCheckerTests: XCTestCase {
     func testPingWithIPHost(){
         
         let expectation = XCTestExpectation(description: "Running test with \(IPHost)")
-        PingChecker.pingHostname(hostname: IPHost, andResultCallback: {latency in
+        PingChecker.pingHostname(hostname: IPHost, resultCallback: { latency in
       
             guard let latencyMS = latency else { return }
             XCTAssertTrue(latencyMS > 0)
+            expectation.fulfill()
+        })
+        wait(for: [expectation], timeout: 5.0)
+    }
+    
+    func testPingFutureWithUnknownHost(){
+        let expectation = XCTestExpectation(description: "Running test with \(UnknownHost)")
+        let pingFuture = PingChecker.pingHostname(hostname: UnknownHost)
+        // cInstance is not used but according to the documentation: Deallocation of the result will tear down the subscription stream.
+        let cInstance = pingFuture.sink(receiveCompletion: {completion in
+            switch completion {
+            case .failure(let error):
+                
+                // Check if the error is of the right type
+                XCTAssertTrue(error is PingChecker.PingCheckerError)
+                // Check if the error has the correct value
+                XCTAssertEqual(error as? PingChecker.PingCheckerError, PingChecker.PingCheckerError.networkUnreachable)
+                expectation.fulfill()
+            case .finished: ()
+               }
+            
+        }, receiveValue: { _ in })
+        wait(for: [expectation], timeout: 5.0)
+    }
+    
+    func testPingFutureWithKnownHost() {
+        let expectation = XCTestExpectation(description: "Running test with \(KnownHost)")
+        let pingFuture = PingChecker.pingHostname(hostname: KnownHost)
+        // cInstance is not used but according to the documentation: Deallocation of the result will tear down the subscription stream.
+        let cInstance = pingFuture.sink(receiveCompletion: {_ in }, receiveValue: {
+            latency in
+            XCTAssertTrue(latency > 0)
             expectation.fulfill()
         })
         wait(for: [expectation], timeout: 5.0)
