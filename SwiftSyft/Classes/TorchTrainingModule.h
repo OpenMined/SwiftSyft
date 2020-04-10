9d90e4562fdbefc9f71668584bc0758134433c73
mjjimenez <mjjimenez87@gmail.com>
2020-04-10 16:14:32 +0800

    Fix warning on designated initializer not implemented

--- SwiftSyft/Classes/TorchTrainingModule.h
+++ SwiftSyft/Classes/TorchTrainingModule.h
@@ -13,7 +13,8 @@ NS_ASSUME_NONNULL_BEGIN
 
 - (instancetype)initWithFileAtPath:(NSString*)filePath
 NS_SWIFT_NAME(init(fileAtPath:))NS_DESIGNATED_INITIALIZER;
-
++ (instancetype)new NS_UNAVAILABLE;
+- (instancetype)init NS_UNAVAILABLE;
 
 @end
 
