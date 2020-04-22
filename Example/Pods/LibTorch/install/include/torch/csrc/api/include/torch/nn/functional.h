c33df973904401fc7a44bf40078096af391ab31e
mjjimenez <mjjimenez87@gmail.com>
2020-04-22 23:43:04 +0800

    Deleted `Pods` folder because LibTorch library is larger than 100 MB (Github limit).

--- Example/Pods/LibTorch/install/include/torch/csrc/api/include/torch/nn/functional.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-#include <torch/nn/functional/distance.h>
-#include <torch/nn/functional/pooling.h>
--- Example/Pods/Target Support Files/SyftProto/SyftProto-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SyftProto : NSObject
-@end
-@implementation PodsDummy_SyftProto
-@end
