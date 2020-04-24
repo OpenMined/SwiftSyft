80258d7aeda5c0353041cb376d073a39c84648fb
mjjimenez <mjjimenez87@gmail.com>
2020-04-24 22:41:54 +0800

    Added LineChartViewController and linked line chart view

---
+++ Example/SwiftSyft/LossChartViewController.swift
@@ -0,0 +1,32 @@
+//
+//  LossChartViewController.swift
+//  SwiftSyft_Example
+//
+//  Created by Mark Jeremiah Jimenez on 24/04/2020.
+//  Copyright © 2020 CocoaPods. All rights reserved.
+//
+
+import UIKit
+import Charts
+
+class LossChartViewController: UIViewController {
+
+    @IBOutlet weak var lineChartView: LineChartView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
