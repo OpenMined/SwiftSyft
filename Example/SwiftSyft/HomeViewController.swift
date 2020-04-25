3c09a1a4e39e5afaf3f0c0cc2fa6bd91d7e442c6
mjjimenez <mjjimenez87@gmail.com>
2020-04-25 23:24:30 +0800

    Removed previous UI used for webrtc connection

--- Example/SwiftSyft/HomeViewController.swift
+++ Example/SwiftSyft/HomeViewController.swift
@@ -20,28 +20,19 @@ enum StaticHomeScreenStrings {
     static let swiftSyftURL = "https://github.com/OpenMined/SwiftSyft"
     static let openMined = "OpenMined"
     static let openMinedKey = "@OpenMined@"
-    static let openMinedURL = "https://github.com/OpenMined/grid.js"
+    static let openMinedURL = "https://github.com/OpenMined/PyGrid/"
     static let socketURL = "ws://127.0.0.1:3000" // "wss://localhost:3000/"
-    static let protocolID = "50801316202"
-    static let connectButtonText = "Connect to grid.js server"
-    static let disconnectButtonText = "Disconnect grid.js server"
-    static let messagePlaceholder = "Enter your message ..."
-    static let sendButtonText = "Send message"
+    static let connectButtonText = "Connect to PyGrid server"
 }
 
 class HomeViewController: UIViewController, UITextViewDelegate {
-    var socket: SocketClientProtocol!
-    var syftRTCClient: SyftRTCClient!
     var isConnected = false
 
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var headerDescriptionLabel: UILabel!
     @IBOutlet weak var descriptionTextView: UITextView!
     @IBOutlet weak var socketURLTextField: UITextField!
-    @IBOutlet weak var protocolIDTextField: UITextField!
     @IBOutlet weak var connectButton: UIButton!
-    @IBOutlet weak var messageTextView: UITextView!
-    @IBOutlet weak var sendButton: UIButton!
 
     private var syftJob: SyftJob?
     private var syftClient: SyftClient?
@@ -68,24 +59,11 @@ class HomeViewController: UIViewController, UITextViewDelegate {
                         url: StaticHomeScreenStrings.openMinedURL)
 
         socketURLTextField.text = StaticHomeScreenStrings.socketURL
-        protocolIDTextField.text = StaticHomeScreenStrings.protocolID
-
-        messageTextView.text = StaticHomeScreenStrings.messagePlaceholder
-        messageTextView.textColor = UIColor.lightGray
-        messageTextView.delegate = self
-        sendButton.titleLabel?.text = StaticHomeScreenStrings.sendButtonText
-
-        showServerConnectedUI(isConnected)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-
-    deinit {
-      socket.disconnect()
-      socket.delegate = nil
     }
 
     @IBAction func connectPressed(_ sender: UIButton) {
@@ -140,48 +118,6 @@ class HomeViewController: UIViewController, UITextViewDelegate {
         }
     }
 
-    @IBAction func sendPressed(_ sender: Any) {
-        guard !messageTextView.text!.isEmpty else {
-            print("Message is empty!")
-            return
-        }
-        socket.sendText(text: messageTextView.text)
-        messageTextView.text = StaticHomeScreenStrings.messagePlaceholder
-        messageTextView.textColor = UIColor.lightGray
-    }
-
-    func showServerConnectedUI(_ isConnected: Bool) {
-        let hideUIElements = !isConnected
-        messageTextView.isHidden = hideUIElements
-        sendButton.isHidden = hideUIElements
-        connectButton.titleLabel?.text = isConnected ?
-            StaticHomeScreenStrings.disconnectButtonText :
-            StaticHomeScreenStrings.connectButtonText
-    }
-
-    // MARK: - TextView behavior
-    func textViewDidBeginEditing(_ textView: UITextView) {
-        if textView.textColor == UIColor.lightGray {
-            textView.text = nil
-            textView.textColor = UIColor.black
-        }
-    }
-
-    func textViewDidEndEditing(_ textView: UITextView) {
-        if textView.text.isEmpty {
-            textView.text = StaticHomeScreenStrings.messagePlaceholder
-            textView.textColor = UIColor.lightGray
-        }
-    }
-
-    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
-        if text == "\n" {
-            textView.resignFirstResponder()
-            return false
-        }
-        return true
-    }
-
     @objc func adjustForKeyboard(notification: Notification) {
         guard let keyboardValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey]
             as? NSValue else { return }
@@ -190,7 +126,7 @@ class HomeViewController: UIViewController, UITextViewDelegate {
         let keyboardViewEndFrame = view.convert(keyboardScreenEndFrame, from: view.window)
 
         if notification.name == UIResponder.keyboardWillHideNotification {
-            messageTextView.contentInset = .zero
+            self.scrollView.contentInset = .zero
         } else {
             let userInfo = notification.userInfo!
             var keyboardFrame: CGRect = (userInfo[UIResponder.keyboardFrameBeginUserInfoKey]
@@ -204,11 +140,6 @@ class HomeViewController: UIViewController, UITextViewDelegate {
                                                    bottom: keyboardViewEndFrame.height - view.safeAreaInsets.bottom,
                                                    right: 0)
         }
-
-        messageTextView.scrollIndicatorInsets = messageTextView.contentInset
-
-        let selectedRange = messageTextView.selectedRange
-        messageTextView.scrollRangeToVisible(selectedRange)
     }
 }
 
@@ -220,22 +151,5 @@ extension NSAttributedString {
 
         mutableAttr.replaceCharacters(in: placeholderRange, with: linkAttr)
         return mutableAttr
-    }
-}
-
-// MARK: - SocketClientDelegate
-extension HomeViewController: SocketClientDelegate {
-    func didConnect(_ socketClient: SocketClientProtocol) {
-        isConnected = true
-        showServerConnectedUI(isConnected)
-    }
-
-    func didDisconnect(_ socketClient: SocketClientProtocol) {
-        isConnected = false
-        showServerConnectedUI(isConnected)
-    }
-
-    func didReceive(socketMessage result: Result<Data, Error>) {
-        //
     }
 }
