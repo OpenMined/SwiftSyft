92e8a92626e2257385156e29c607641ce8aa2696
mjjimenez <mjjimenez87@gmail.com>
2020-05-08 17:10:38 +0800

    Added new project to demonstrate using SwiftSyft using background task scheduler

---
+++ Example/SwiftSyft-Background/SwiftSyft-Background/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftSyft-Background
+//
+//  Created by Mark Jeremiah Jimenez on 08/05/2020.
+//  Copyright © 2020 OpenMined. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
