b4ddfdb7978e2f6e9885154f26d3cf0c1402ef7a
mjjimenez <mjjimenez87@gmail.com>
2020-05-10 22:24:49 +0800

    Added MNIST training code to execute for registerd background task

--- Example-Background/SwiftSyft-Background/AppDelegate.swift
+++ Example-Background/SwiftSyft-Background/AppDelegate.swift
@@ -7,29 +7,78 @@
 //
 
 import UIKit
+import SwiftSyft
+import BackgroundTasks
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    private var syftJob: SyftJob?
+    private var syftClient: SyftClient?
+    private var backgroundTaskCancelled = false
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+
+        BGTaskScheduler.shared.register(forTaskWithIdentifier: "", using: DispatchQueue.global()) { task in
+
+            self.executeSyftJob(backgroundTask: task)
+
+        }
+
         return true
     }
 
-    // MARK: UISceneSession Lifecycle
+    func executeSyftJob(backgroundTask: BGTask) {
 
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
+        // Initate federated cycle request
+        guard let syftClient = SyftClient(url: URL(string: "ws://127.0.0.1:5000")!) else {
+            backgroundTask.setTaskCompleted(success: false)
+            return
+        }
 
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+        self.syftJob = syftClient.newJob(modelName: "mnist", version: "1.0.0")
+        self.syftJob?.onReady(execute: { plan, clientConfig, modelReport in
+
+            do {
+
+                let (mnistData, labels) = try MNISTLoader.load(setType: .train, batchSize: clientConfig.batchSize)
+
+                for case let (batchData, labels) in zip(mnistData, labels) {
+
+                    guard !self.backgroundTaskCancelled else {
+                        return
+                    }
+
+                    let flattenedBatch = MNISTLoader.flattenMNISTData(batchData)
+                    let oneHotLabels = MNISTLoader.oneHotMNISTLabels(labels: labels).compactMap { Float($0)}
+
+                    let trainingData = try TrainingData(data: flattenedBatch, shape: [clientConfig.batchSize, 784])
+                    let validationData = try ValidationData(data: oneHotLabels, shape: [clientConfig.batchSize, 10])
+
+                    plan.execute(trainingData: trainingData, validationData: validationData, clientConfig: clientConfig)
+                }
+
+                let diffStateData = try plan.generateDiffData()
+                modelReport(diffStateData)
+
+                backgroundTask.setTaskCompleted(success: true)
+
+            } catch let error {
+                debugPrint(error.localizedDescription)
+                backgroundTask.setTaskCompleted(success: false)
+            }
+
+        })
+        self.syftJob?.onError(execute: { error in
+            print(error.localizedDescription)
+            backgroundTask.setTaskCompleted(success: false)
+        })
+        self.syftJob?.start()
+        self.syftClient = syftClient
+        backgroundTask.expirationHandler = {
+            self.backgroundTaskCancelled = true
+        }
     }
 
 
--- Example/SwiftSyft/HomeViewController.swift
+++ Example/SwiftSyft/HomeViewController.swift
@@ -71,7 +71,7 @@ class HomeViewController: UIViewController, UITextViewDelegate {
 
     @IBAction func connectPressed(_ sender: UIButton) {
 
-        // Initate federated cylcle request
+        // Initate federated cycle request
         if let syftClient = SyftClient(url: URL(string: "ws://127.0.0.1:5000")!) {
 
             // Show loading UI
