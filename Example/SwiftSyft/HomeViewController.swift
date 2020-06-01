1229aedafcffbf48509549d49216d868f342df69
mjjimenez <mjjimenez87@gmail.com>
2020-06-01 16:59:37 +0800

    Added more comments for the MNIST training example

--- Example/SwiftSyft/HomeViewController.swift
+++ Example/SwiftSyft/HomeViewController.swift
@@ -71,25 +71,42 @@ class HomeViewController: UIViewController, UITextViewDelegate {
 
     @IBAction func connectPressed(_ sender: UIButton) {
 
-        // Initate federated cycle request
+        // This is a demonstration of how to use SwiftSyft with PyGrid to train a plan on local data on an iOS device
+
+        // Create a client with a PyGrid server URL
         if let syftClient = SyftClient(url: URL(string: "ws://127.0.0.1:5000")!) {
+
+            // Store the client as a property so it doesn't get deallocated during training.
+            self.syftClient = syftClient
 
             // Show loading UI
             self.connectButton.isHidden = true
             self.loadingLabel.isHidden = false
             self.activityIndicator.startAnimating()
 
+            // Create a new federated learning job with the model name and version
             self.syftJob = syftClient.newJob(modelName: "mnist", version: "1.0.0")
+
+            // This function is called when SwiftSyft has downloaded the plans and model parameters from PyGrid
+            // You are ready to train your model on your data
+            // plan - Use this to generate diffs using our training data
+            // clientConfig - contains the configuration for the training cycle (batchSize, learning rate) and metadata for the model (name, version)
+            // modelReport - Used as a completion block and reports the diffs to PyGrid.
             self.syftJob?.onReady(execute: { plan, clientConfig, modelReport in
 
+                // Set label to show that MNIST data is currently being loaded into memory
                 DispatchQueue.main.sync {
                     self.loadingLabel.text = "Loading MNIST Data"
                 }
 
                 do {
 
+                    // This returns a lazily evaluated sequence for each MNIST image and the corresponding label
+                    // It divides the training data and the label by batches
                     let (mnistData, labels) = try MNISTLoader.load(setType: .train, batchSize: clientConfig.batchSize)
 
+                    // Stop the loading indicator and present the line chart view controller
+                    // plotting the loss from training
                     DispatchQueue.main.sync {
 
                         self.loadingLabel.isHidden = true
@@ -105,17 +122,28 @@ class HomeViewController: UIViewController, UITextViewDelegate {
 
                     }
 
+                    // Iterate through each batch of MNIST data and label
                     for case let (batchData, labels) in zip(mnistData, labels) {
 
+                        // We need to create an autorelease pool to release the training data from memory after each loop
                         try autoreleasepool {
 
+                            // Preprocess MNIST data by flattening all of the MNIST batch data as a single array
                             let flattenedBatch = MNISTLoader.flattenMNISTData(batchData)
+
+                            // Preprocess the label ( 0 to 9 ) by creating one-hot features and then flattening the entire thing
                             let oneHotLabels = MNISTLoader.oneHotMNISTLabels(labels: labels).compactMap { Float($0)}
 
+                            // Since we don't have native tensor wrappers in Swift yet, we use `TrainingData` and `ValidationData`
+                            // classes to store the data and shape.
                             let trainingData = try TrainingData(data: flattenedBatch, shape: [clientConfig.batchSize, 784])
                             let validationData = try ValidationData(data: oneHotLabels, shape: [clientConfig.batchSize, 10])
 
+                            // Execute the plan with the training data and validation data. `plan.execute()` returns the loss and you can use
+                            // it if you want to (plan.execute() has the @discardableResult attribute)
                             let loss = plan.execute(trainingData: trainingData, validationData: validationData, clientConfig: clientConfig)
+
+                            // Pass the loss to the line chart view controller to plot.
                             self.lossSubject?.send(loss)
                             print("loss: \(loss)")
 
@@ -123,21 +151,29 @@ class HomeViewController: UIViewController, UITextViewDelegate {
 
                     }
 
+                    // Generate diff data and report the final diffs as
                     let diffStateData = try plan.generateDiffData()
                     modelReport(diffStateData)
 
                     self.lossSubject?.send(completion: .finished)
 
                 } catch let error {
+
+                    // Handle any error from the training cycle
                     debugPrint(error.localizedDescription)
+
                 }
 
             })
+
+            // This is the error handler for any job exeuction errors like connecting to PyGrid
             self.syftJob?.onError(execute: { error in
                 print(error)
             })
+
+            // Start the job. You can set that the job should only execute if the device is being charge and there is a WiFi connection.
+            // These options are true by default if you don't specify them.
             self.syftJob?.start(chargeDetection: false, wifiDetection: false)
-            self.syftClient = syftClient
         }
     }
 
