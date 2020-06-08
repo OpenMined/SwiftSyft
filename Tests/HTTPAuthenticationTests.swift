d8d3dbecb3505b6329d9be8d862e42dde31f642f
mjjimenez <mjjimenez87@gmail.com>
2020-06-08 15:58:10 +0800

    Added initial authentication test verifyin that no authentication flow works.

--- Tests/HTTPAuthenticationTests.swift
+++ Tests/HTTPAuthenticationTests.swift
@@ -11,31 +11,80 @@ import OHHTTPStubs
 
 class HTTPAuthenticationTests: XCTestCase {
 
+    enum AuthenticationMode {
+        case noAuthentication
+        case withAuthentication
+    }
+
+    var authMode: AuthenticationMode!
+
+    var cycleRequestCalled: Bool!
+    var cycleRequestExpectation: XCTestExpectation!
+
+    var syftClient: SyftClient!
+    var syftJob: SyftJob!
+
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
-        stub(condition: isHost("test.com")) { request -> HTTPStubsResponse in
-            
-            request.url?.scheme
+        stub(condition: isHost("test.com") && isPath("/federated/authenticate")) { request -> HTTPStubsResponse in
+
+            let responseFile = OHPathForFile("authenticate-success.json", type(of: self))!
+
+            return HTTPStubsResponse(fileAtPath: responseFile, statusCode: 200, headers: nil)
+
         }
 
+        stub(condition: isHost("test.com") && isPath("/federated/speed-test")) { request -> HTTPStubsResponse in
+
+            return HTTPStubsResponse(data: Data(), statusCode: 200, headers: nil)
+        }
+
+        stub(condition: isHost("test.com") && isPath("/federated/cycle-request")) { request -> HTTPStubsResponse in
+
+            self.cycleRequestExpectation.fulfill();
+
+            return HTTPStubsResponse(error: URLError.init(URLError.Code.cancelled))
+        }
+
+
+
+    }
+
+    func testNoAuth() {
+
+        authMode = .noAuthentication
+        cycleRequestCalled = false
+
+        self.cycleRequestExpectation = expectation(description: "test auth if successful")
+
+        self.syftClient = SyftClient(url: URL(string: "http://test.com:5000")!)!
+        self.syftJob = syftClient.newJob(modelName: "mnist", version: "1.0")
+        self.syftJob.onError { _ in
+            XCTFail("Syft Authentication Failed")
+        }
+        self.syftJob.start(chargeDetection: false, wifiDetection: false)
+
+        wait(for: [self.cycleRequestExpectation], timeout: 5)
+
+    }
+
+    func test_with_auth_no_token() {
+
+    }
+
+    func test_with_auth_invalid_token() {
+
+    }
+
+    func test_with_auth_valid_token() {
 
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+        HTTPStubs.removeAllStubs()
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
     }
 
 }
