0ad0c1904542ee669cf22c3eb96aeaac22ae868d
mjjimenez <mjjimenez87@gmail.com>
2020-06-15 21:22:13 +0800

    Reset network stubs at end of test

--- Tests/WebSocketAuthentication.swift
+++ Tests/WebSocketAuthentication.swift
@@ -182,6 +182,9 @@ class WebSocketAuthentication: XCTestCase {
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+
+        HTTPStubs.removeAllStubs()
+
     }
 
 
