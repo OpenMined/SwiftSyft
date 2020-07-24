80703c01a1620babe8a6a7f7b45cd77fce35ee26
Rohith-hacker <cnu.sunnypudari@gmail.com>
2020-07-24 15:06:43 +0530

    added test file for syftclient

---
+++ Tests/SyftClientTests.swift
@@ -0,0 +1,38 @@
+//
+//  SyftClientTests.swift
+//  OpenMinedSwiftSyft-Unit-Tests
+//
+//  Created by Rohith Pudari on 19/07/20.
+//
+
+import XCTest
+@testable import SwiftSyft
+
+class SyftClientTests: XCTestCase {
+    let authToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.e30.Cn_0cSjCw1QKtcYDx_mYN_q9jO2KkpcUoiVbILmKVB4LUCQvZ7YeuyQ51r9h3562KQoSas_ehbjpz2dw1Dk24hQEoN6ObGxfJDOlemF5flvLO_sqAHJDGGE24JRE4lIAXRK6aGyy4f4kmlICL6wG8sGSpSrkZlrFLOVRJckTptgaiOTIm5Udfmi45NljPBQKVpqXFSmmb3dRy_e8g3l5eBVFLgrBhKPQ1VbNfRK712KlQWs7jJ31fGpW2NxMloO1qcd6rux48quivzQBCvyK8PV5Sqrfw_OMOoNLcSvzePDcZXa2nPHSu3qQIikUdZIeCnkJX-w0t8uEFG3DfH1fVA"
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testnewjob() {
+        XCTAssertNotNil(SyftClient.init(url: URL(string: "ws://127.0.0.1:5000")!, authToken: authToken))
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
