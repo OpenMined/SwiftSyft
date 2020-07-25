1ea20a9b737037ed1e54d074e96bc42f87e33a98
Rohith-hacker <cnu.sunnypudari@gmail.com>
2020-07-25 15:04:04 +0530

    added support for web socket scheme

--- Tests/SyftClientTests.swift
+++ Tests/SyftClientTests.swift
@@ -19,6 +19,8 @@ class SyftClientTests: XCTestCase {
         XCTAssertNotNil(SyftClient(url: URL(string: "http://www.test.com")!))
         XCTAssertNotNil(SyftClient(url: URL(string: "ws://test.com:5000")!))
         XCTAssertNotNil(SyftClient(url:URL(string: "ws://test.com:5000")!, authToken: "test_auth"))
+        XCTAssertNotNil(SyftClient(url: URL(string: "wss://test.com:5000")!))
+        XCTAssertNotNil(SyftClient(url:URL(string: "wss://test.com:5000")!, authToken: "test_auth"))
     }
     func test_newjob_ws_Auth(){
         let syftClient_ws_Auth = SyftClient(url: URL(string: "ws://test.com:5000")!, authToken: "test_auth")
@@ -48,6 +50,14 @@ class SyftClientTests: XCTestCase {
         let syftClient_https_Noauth = SyftClient(url: URL(string: "https://test.com")!)
             XCTAssertNotNil(syftClient_https_Noauth?.newJob(modelName: "mnist", version: "0.1"))
         }
+    func test_newjob_wss_Auth(){
+        let syftClient_wss_Auth = SyftClient(url: URL(string: "wss://test.com:5000")!, authToken: "test_auth")
+        XCTAssertNotNil(syftClient_wss_Auth?.newJob(modelName: "mnist", version: "1.0.0-A"))
+    }
+    func test_newjob_wss_NoAuth(){
+        let syftClient_wss_NoAuth = SyftClient(url: URL(string: "wss://test.com:5000")!)
+        XCTAssertNotNil(syftClient_wss_NoAuth?.newJob(modelName: "mn-st", version: "1.0.0.bc"))
+    }
     
   //  func testPerformanceExample() throws {
         // This is an example of a performance test case.
