4d72879bd21cdbabe6a52d019bcf0b5b37862b43
mjjimenez <mjjimenez87@gmail.com>
2020-08-15 22:44:05 +0800

    Added convenience function to map errors for Combine decode

--- SwiftSyft/Classes/SwiftSyftError.swift
+++ SwiftSyft/Classes/SwiftSyftError.swift
@@ -1,4 +1,5 @@
 import Foundation
+import Combine
 
 enum SwiftSyftError: Error {
     case networkConstraintsFailure
@@ -9,3 +10,17 @@ enum SwiftSyftError: Error {
     case unknownError(underlyingError: Error?)
 }
 
+// Easier `mapError` for decode publisher
+// Source: https://stackoverflow.com/questions/57768427/swift-combine-chaining-maperror
+extension Publisher {
+    //swiftlint:disable line_length
+    func decode<Item, Coder>(type: Item.Type,
+                             decoder: Coder,
+                             errorTransform: @escaping (Error) -> Failure) -> Publishers.FlatMap<Publishers.MapError<Publishers.Decode<Just<Self.Output>, Item, Coder>, Self.Failure>, Self> where Item: Decodable, Coder: TopLevelDecoder, Self.Output == Coder.Input {
+        return flatMap {
+            Just($0)
+                .decode(type: type, decoder: decoder)
+                .mapError { errorTransform($0) }
+        }
+    }
+}
