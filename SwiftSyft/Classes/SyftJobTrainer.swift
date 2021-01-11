c5d75c1986824af78d44d6e23676e2a745bf6922
mjjimenez <mjjimenez87@gmail.com>
2021-01-11 18:54:37 +0800

    Added main training loop for job trainer

--- SwiftSyft/Classes/SyftJobTrainer.swift
+++ SwiftSyft/Classes/SyftJobTrainer.swift
@@ -28,36 +28,91 @@ public class SyftJobTrainer {
     init(jobEventPublisher: AnyPublisher<SyftJobEvents, Error>) {
         jobEventPublisher.sink { result in
             print(result)
-        } receiveValue: { jobEvent in
-            print(jobEvent)
+        } receiveValue: { [unowned self] jobEvent in
+
+            switch jobEvent {
+            case .onReady(let model, planDictionary: let planDictionary, let clientConfig, let modelReport):
+
+                self.startTraining(model: model, planDictionary: planDictionary, clientConfig: clientConfig, report: modelReport)
+
+            case .onRejected(timeout: _):
+                // TODO: Add error handlers
+                print("job trainer rejected")
+            case .onError(error: _):
+                // TODO: Add error handlers
+                print("job trainer error")
+            }
+
         }.store(in: &self.disposeBag)
     }
 
-    func onStarted(closure: @escaping () -> Void) {
+    func startTraining(model: SyftModel, planDictionary: [String: TorchModule], clientConfig: FederatedClientConfig, report: ModelReport) {
+
+        self.observations.started.forEach { closure in
+            closure()
+        }
+
+        for epoch in 0..<clientConfig.maxEpochs {
+
+            var batchIdx = 0
+
+            self.observations.epochStart.forEach { closure in
+                closure(epoch)
+            }
+
+            for batch in 0...10 {
+
+                self.observations.batchStart.forEach { closure in
+                    closure(epoch, batch)
+                }
+
+
+
+
+                self.observations.batchEnd.forEach { closure in
+                    closure(epoch, batch)
+                }
+
+                batchIdx += 1
+            }
+
+            self.observations.epochEnd.forEach { closure in
+                closure(epoch)
+            }
+
+        }
+
+        self.observations.ended.forEach { closure in
+            closure()
+        }
+
+    }
+
+    public func onStarted(closure: @escaping () -> Void) {
         self.observations.started.append(closure)
     }
 
-    func onEpochStart(closure: @escaping (Int) -> Void) {
+    public func onEpochStart(closure: @escaping (Int) -> Void) {
         self.observations.epochStart.append(closure)
     }
 
-    func onEpochEnd(closure: @escaping (Int) -> Void) {
-        self.observations.epochStart.append(closure)
+    public func onEpochEnd(closure: @escaping (Int) -> Void) {
+        self.observations.epochEnd.append(closure)
     }
 
-    func onBatchStart(closure: @escaping (Int, Int) -> Void) {
+    public func onBatchStart(closure: @escaping (Int, Int) -> Void) {
         self.observations.batchStart.append(closure)
     }
 
-    func onBatchEnd(closure: @escaping (Int, Int) -> Void) {
+    public func onBatchEnd(closure: @escaping (Int, Int) -> Void) {
         self.observations.batchEnd.append(closure)
     }
 
-    func onEnded(closure: @escaping () -> Void) {
+    public func onEnded(closure: @escaping () -> Void) {
         self.observations.ended.append(closure)
     }
 
-    func onError(closure: @escaping (Error) -> Void) {
+    public func onError(closure: @escaping (Error) -> Void) {
         self.observations.error.append(closure)
     }
 
